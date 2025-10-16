36846b5025d1c133f916585a37faa38a12ba6b49
matia <matia@DESKTOP-HA95U4G>
2025-10-16 10:44:34 -0300

    arreglos en main y mapa

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,73 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String nombrejugador;
+		String nombredebase;
+		int opcion=0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("BIENVENIDO AL TUTORIAL DE TOWER DEFENCE");
+		System.out.println("ingresa tu nombre para continuar");
+		do {
+			nombrejugador =sc.nextLine();
+		if(nombrejugador.trim().isEmpty()) {
+			System.out.println("no ingresaste un nombre. Intentalo de nuevo");
+		}
+		}while(nombrejugador.trim().isEmpty());
+		Jugador jugador = new Jugador(nombrejugador);
+		System.out.println("hola "+nombrejugador+". Es hora de defender tu base");
+		
+		System.out.println("ingresa el nombre de tu base");
+		do {
+			nombredebase =sc.nextLine();
+		if(nombredebase.trim().isEmpty()) {
+			System.out.println("no ingresaste un de tu base. Intentalo de nuevo");
+		}
+		}while(nombredebase.trim().isEmpty());
+		Base nuevabase = new Base(nombredebase,1000);
+		System.out.println("tu base principal se llama "+nombredebase);
+		System.out.println("TE PASASTE CON LOS NOMBRES PIPO");
+		System.out.println("es hora de defender tu base. Los enemigos estan cerca");
+		System.out.println("presiona una tecla para continuar");
+		sc.nextLine();
+		
+		System.out.println("como nunca jugaste un tower defense y esto es un tutorial");
+		System.out.println("solo podras elegir una de las dos posiciones. Norte o Sur");
+		System.out.println("selecciona una de estas dos opciones");
+		
+		do {
+			System.out.println("1)posicion Norte");
+			System.out.println("2)posicion Sur");
+			if(sc.hasNextInt()) {
+				opcion=sc.nextInt();
+				sc.nextLine();
+			}else {
+				System.out.println("Error. Selecciona 1) la posicion Norte o 2) la posicion Sur ");
+				sc.nextLine();
+				opcion=0;
+				continue;
+			}
+			Torre torre1 = new Torre("Torre de flechas",100,15,15,nuevabase);
+			
+			switch (opcion) {
+				case 1:
+					System.out.println("seleccionaste la posicion Norte");
+					
+					
+				break;
+				case 2:
+					System.out.println("seleccionaste la posicion Sur");
+					
+				break;
+			default:
+					System.out.println("Error. Solo puedes elegir la posicion norte o la posicion sur");
+		}
+		}while(opcion!=1 && opcion!=2);
+			
+		
 	}
-
 }
+
+
--- src/Mapa.java
+++ src/Mapa.java
@@ -3,11 +3,13 @@ public class Mapa {
 	private String nombre;
 	private Base base; //AGREGACION
 	private Torre torre;//AGREGACION
+	private String posicionTorre;
 	
-	public Mapa(String nombre, Base base, Torre torre) {
+	public Mapa(String nombre, Base base, Torre torre,String posicionTorre) {
 		this.nombre = nombre;
 		this.base = base;
 		this.torre = torre;
+		this.posicionTorre = posicionTorre;
 	}
 	public void mostrarInfo() {
 		System.out.println("Nombre del mapa: "+nombre);
@@ -16,7 +18,9 @@ public class Mapa {
 		System.out.println("----------------------");
 		System.out.println("Informacion de torre");
 		torre.mostrarInfo();
+		System.out.println("la posicion de la torre es la "+posicionTorre);
 		System.out.println("----------------------");
+		
 		
 	}
 
